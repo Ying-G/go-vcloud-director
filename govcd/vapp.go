1c8056f546d0102cd6955385b40ea83db7f4c58a
yingyguo <32286213+yingyguo@users.noreply.github.com>
2019-01-29 13:53:17 +0800

    Update vapp.go

--- govcd/vapp.go
+++ govcd/vapp.go
@@ -11,7 +11,7 @@ import (
 	"net/url"
 	"strconv"
 
-	"github.com/Ying-G/go-vcloud-director/types/v56"
+	"github.com/yingyguo/go-vcloud-director/types/v56"
 	"github.com/vmware/go-vcloud-director/util"
 )
 
